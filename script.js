dc206eb559008862fc2d22d7be09c415fc7c0ae0
YZeitler <amz61@msn.com>
2017-08-29 18:01:14 +0300

    Both buttons finctional

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 var beers = [];
 
+function updateBeers() {
 
-$('.post-beer').click(function() {
+    $('ul').empty();
+    for (i = 0; i < beers.length; i++) {
+
+        $(".beers-list").append('<li>' + beers[i].name + ", " + beers[i].category + ", Rating: " + beers[i].rating + '</li>');
+
+    }
+}
+
+$('#post').click(function() {
     var name = $('.beer-input').val();
     var category = $('.category-input').val();
+    var rating = $('#select option:selected').text();
 
-    function addBeer(name, category) {
+    function addBeer(name, category, rating) {
 
         const object = new Object();
         object.name = name;
         object.category = category;
+        object.rating = rating;
         beers.push(object);
+
     }
-    addBeer(name, category);
+    addBeer(name, category, rating);
+    updateBeers();
 });
 
-function updateBeers() {
+var ascending = false;
 
+$('#sort').click(function() {
 
-}
+    beers.sort(function(a, b) {
+        return ascending ? a.rating - b.rating : b.rating - a.rating;
+    });
+    updateBeers();
+    ascending = !ascending;
+
+});
