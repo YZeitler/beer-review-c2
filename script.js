68a64955f68eb41a407e668ceb09fcb5cf02ea88
YZeitler <amz61@msn.com>
2017-09-03 16:40:59 +0300

    buttons within function

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-var beers = [];
+(function() {
 
-function updateBeers() {
+    var beers = [];
 
-    $('ul').empty();
-    for (i = 0; i < beers.length; i++) {
+    function updateBeers() {
 
-        $(".beers-list").append('<li>' + beers[i].name + ", " + beers[i].category + ", Rating: " + beers[i].rating + '</li>');
+        $('ul').empty();
+        for (i = 0; i < beers.length; i++) {
 
+            $(".beers-list").append('<li>' + beers[i].name + ", " + beers[i].category + ", Rating: " + beers[i].rating + '</li>');
+
+        }
     }
-}
 
-$('#post').click(function() {
-    var name = $('.beer-input').val();
-    var category = $('.category-input').val();
-    var rating = $('#select option:selected').text();
+    $('#post').click(function() {
+        var name = $('.beer-input').val();
+        var category = $('.category-input').val();
+        var rating = $('#select option:selected').text();
 
-    function addBeer(name, category, rating) {
+        function addBeer(name, category, rating) {
 
-        const object = new Object();
-        object.name = name;
-        object.category = category;
-        object.rating = rating;
-        beers.push(object);
+            const object = new Object();
+            object.name = name;
+            object.category = category;
+            object.rating = rating;
+            beers.push(object);
 
-    }
-    addBeer(name, category, rating);
-    updateBeers();
-});
-
-var ascending = false;
-
-$('#sort').click(function() {
-
-    beers.sort(function(a, b) {
-        return ascending ? a.rating - b.rating : b.rating - a.rating;
+        }
+        addBeer(name, category, rating);
+        updateBeers();
     });
-    updateBeers();
-    ascending = !ascending;
 
-});
+    var ascending = false;
+
+    $('#sort').click(function() {
+
+        beers.sort(function(a, b) {
+            return ascending ? a.rating - b.rating : b.rating - a.rating;
+        });
+        updateBeers();
+        ascending = !ascending;
+
+    });
+})();
